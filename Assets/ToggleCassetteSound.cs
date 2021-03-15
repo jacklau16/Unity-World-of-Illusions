76b01a22243c08693b1266188546e71b8bc0a0a1
jacklau16 <63191844+jacklau16@users.noreply.github.com>
2021-03-15 22:15:36 +0000

    The rooms are built and connected with tunnels

---
+++ Assets/ToggleCassetteSound.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCassetteSound : MonoBehaviour
+{
+    public GameObject soundPlayer;
+    public Material ledMaterial;
+    AudioSource audioSource;
+    bool isPlaying;
+
+    private void Start()
+    {
+        isPlaying = true;
+        audioSource = soundPlayer.GetComponent<AudioSource>();
+    }
+
+    private void OnMouseOver()
+    {
+        //Debug.Log("ToggleCassetteSound: OnMouseOver()!");
+        
+    }
+    private void OnMouseDown()
+    {
+        //Debug.Log("ToggleCassetteSound: MouseClick!");
+        if (isPlaying)
+        {
+            audioSource.Stop();
+            isPlaying = false;
+            ledMaterial.DisableKeyword("_EMISSION");
+        }
+        else
+        {
+            audioSource.Play();
+            isPlaying = true;
+            ledMaterial.EnableKeyword("_EMISSION");
+        }
+    }
+}
